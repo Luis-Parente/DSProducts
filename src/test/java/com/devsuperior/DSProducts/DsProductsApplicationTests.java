87d6a31a5cb07cf9230051021c3e217a462c18cf
Luis Parente <lgsp25@hotmail.com>
2025-07-11 20:01:47 -0300

    Initial setup

---
+++ src/test/java/com/devsuperior/DSProducts/DsProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
